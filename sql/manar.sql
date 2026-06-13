e3b01e94504724ee9c1a312eb2b98131a4dc1dd8
WilliamvVargas <william.v.vargas@outlook.com>
2026-06-13 18:29:03 -0400

    Estructura base de datos mysql

---
+++ sql/manar.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 14-06-2026 a las 00:15:58
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Base de datos: `mrp_manar`
+--
+CREATE DATABASE IF NOT EXISTS `mrp_manar` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mrp_manar`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login_intentos`
+--
+
+DROP TABLE IF EXISTS `login_intentos`;
+CREATE TABLE `login_intentos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `ip` varchar(45) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` varchar(36) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `nombres` varchar(128) NOT NULL,
+  `apellidos` varchar(128) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Disparadores `usuarios`
+--
+DROP TRIGGER IF EXISTS `antes_insertar_usuarios`;
+DELIMITER $$
+CREATE TRIGGER `antes_insertar_usuarios` BEFORE INSERT ON `usuarios` FOR EACH ROW BEGIN
+    SET NEW.id = UUID();
+END
+$$
+DELIMITER ;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `login_intentos`
+--
+ALTER TABLE `login_intentos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_ip_usuario_fecha` (`ip`,`usuario`,`created_at`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uq_usuarios_usuario` (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `login_intentos`
+--
+ALTER TABLE `login_intentos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
